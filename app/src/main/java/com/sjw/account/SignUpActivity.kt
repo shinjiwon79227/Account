794ca223ddb61f4b0cd078ffc4b27769e99534cc
shinjiwon79227 <shinjiwon79227@gmail.com>
2024-06-25 20:17:12 +0900

    [수정] Lv.2

--- app/src/main/java/com/sjw/account/SignUpActivity.kt
+++ app/src/main/java/com/sjw/account/SignUpActivity.kt
@@ -1,6 +1,7 @@
 package com.sjw.account
 
 import android.os.Bundle
+import android.widget.EditText
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -16,5 +17,11 @@ class SignUpActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val name = findViewById<EditText>(R.id.up_input_name)
+        val id = findViewById<EditText>(R.id.up_input_id)
+        val pw = findViewById<EditText>(R.id.up_input_pw)
+
+
     }
 }
