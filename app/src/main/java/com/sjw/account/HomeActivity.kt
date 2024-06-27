00beab0c71f7d1e50279d02f641fb978b58dbd56
shinjiwon79227 <shinjiwon79227@gmail.com>
2024-06-27 15:35:17 +0900

    [완료] Lv.3

--- app/src/main/java/com/sjw/account/HomeActivity.kt
+++ app/src/main/java/com/sjw/account/HomeActivity.kt
@@ -1,10 +1,8 @@
 package com.sjw.account
 
-import android.net.Uri
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
-import android.widget.ImageView
 import android.widget.TextView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
@@ -17,11 +15,6 @@ class HomeActivity : AppCompatActivity() {
     val nameData = arrayListOf("아찔한 뒷태 김밥","CUTE 김밥","코코낸내 김밥","뭘 봐 김밥","응애 김밥")
     val ageData = arrayListOf("1","2","3","4","999")
     val mbtiData = arrayListOf("ISTP","ENFJ","INTJ","INFP","ENFP")
-    val imgSrcData = arrayListOf(
-        R.drawable.grizzly,
-        R.drawable.panda,
-        R.drawable.icebear
-    )
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,15 +32,12 @@ class HomeActivity : AppCompatActivity() {
         val age = findViewById<TextView>(R.id.home_age)
         val mbti = findViewById<TextView>(R.id.home_MBTI)
 
-        val img = findViewById<ImageView>(R.id.home_img)
-
         val finishBtn = findViewById<Button>(R.id.home_finish_btn)
 
         id.text = inputId
-        name.text = nameData.get(random.nextInt(nameData.size))
-        age.text = ageData.get(random.nextInt(ageData.size))
-        mbti.text = mbtiData.get(random.nextInt(mbtiData.size))
-        img.setImageResource(imgSrcData.get(random.nextInt(imgSrcData.size)))
+        name.text = nameData.get(random.nextInt(nameData.lastIndex))
+        age.text = ageData.get(random.nextInt(ageData.lastIndex))
+        mbti.text = mbtiData.get(random.nextInt(mbtiData.lastIndex))
 
         finishBtn.setOnClickListener { finish() }
     }
