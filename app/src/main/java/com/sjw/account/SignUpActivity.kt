a4107098df5f947a3cdb11708f8ad0c97dcf46cc
shinjiwon79227 <shinjiwon79227@gmail.com>
2024-06-27 14:58:51 +0900

    [완료] Lv.2

--- app/src/main/java/com/sjw/account/SignUpActivity.kt
+++ app/src/main/java/com/sjw/account/SignUpActivity.kt
@@ -23,5 +23,46 @@ class SignUpActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        val name = findViewById<EditText>(R.id.up_input_name)
+        val id = findViewById<EditText>(R.id.up_input_id)
+        val pw = findViewById<EditText>(R.id.up_input_pw)
+
+        img = findViewById(R.id.up_img)
+
+        val signUpBtn = findViewById<Button>(R.id.up_sign_up_btn)
+
+        img.setImageResource(R.drawable.sign_up_img)
+
+        isFocused(name)
+        isFocused(id)
+        isFocused(pw)
+
+        signUpBtn.setOnClickListener {
+            when {
+                name.text.toString().isEmpty() || id.text.toString().isEmpty() || pw.text.toString().isEmpty() -> {
+                    Toast.makeText(this, "입력되지 않은 정보가 있습니다", Toast.LENGTH_SHORT).show()
+                    with(img) {
+                        setImageResource(R.drawable.error_img)
+                        scaleType = ImageView.ScaleType.CENTER_CROP
+                    }
+                }
+                else -> {
+                    Toast.makeText(this, "회원가입 성공!\n당신은 이제 김사모 회원~", Toast.LENGTH_SHORT).show()
+                    finish()
+                }
+            }
+        }
+    }
+
+    private fun isFocused(view: EditText) {
+        view.setOnFocusChangeListener { v, hasFocus ->
+            if (hasFocus) {
+                with(img) {
+                    setImageResource(R.drawable.lupin)
+                    scaleType = ImageView.ScaleType.FIT_XY
+                }
+            }
+        }
     }
 }
